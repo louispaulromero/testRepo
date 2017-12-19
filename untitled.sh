13bf8ca7aea0a449833fbc1bdd386411727045d6
Louis Paul Romero <louispaulromero@gmail.com>
2017-12-18 18:16:52 -0800

    practice

---
+++ untitled.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+####################################
+#
+# Great script!
+#https://help.ubuntu.com/lts/serverguide/backup-shellscripts.html 
+####################################
+DATE=%Y-%m-%d
+cd current
+mv tester.txt
+git pull
