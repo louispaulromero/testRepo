d2b72777dd353974702eaec9df7b46361af68be3
Louis Paul Romero <louispaulromero@gmail.com>
2017-12-18 20:49:17 -0800

    test

--- untitled.sh
+++ untitled.sh
@@ -2,9 +2,23 @@
 ####################################
 #
 # Great script!
-#https://help.ubuntu.com/lts/serverguide/backup-shellscripts.html 
+#https://help.ubuntu.com/lts/serverguide/backup-shellscripts.html
 ####################################
-DATE=%Y-%m-%d
-cd current
-mv tester.txt
-git pull
+DATE=`date +%Y-%m-%d`
+OUTPUT="$(git pull)"
+STRING="Already up-to-date."
+if [[ $OUTPUT != $STRING ]]
+then
+	cd old
+	DIRECTORY="$(pwd)"
+	cd ..
+	cd current
+	mkdir -p /$DIRECTORY/$DATE/
+	mv tester.txt $_
+	echo "Both Strings not Equal"
+	echo $OUTPUT
+	echo $STRING
+	echo $DATE
+#else
+	#echo "Both Strings are not Equal"
+fi
